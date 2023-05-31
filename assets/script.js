2107c5829e311aeaed20d927cbe9ded9d7d43912
jdesiderio <desiderio.janyce@gmail.com>
2023-05-31 14:59:57 +0200

    script V1: functioning carousel

--- assets/script.js
+++ assets/script.js
@@ -6,19 +6,23 @@ let index = 0;
 const slides = [
 	{
 		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>",
+		"divDots":'<div class="dot dot_selected"></div><div class="dot"></div><div class="dot"></div><div class="dot"></div>'
 	},
 	{
 		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+		"divDots":'<div class="dot"></div><div class="dot dot_selected"></div><div class="dot"></div><div class="dot"></div>'
 	},
 	{
 		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>",
+		"divDots":'<div class="dot"></div><div class="dot"></div><div class="dot dot_selected"></div><div class="dot"></div>'
 	},
 	{
 		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>",
+		"divDots":'<div class="dot"></div><div class="dot"></div><div class="dot"></div><div class="dot dot_selected"></div>'
 	}
 ]
 
@@ -37,8 +41,30 @@ function imgChange(e) {
 	else {
 		index--;
 	}
+
+	loop();
 	
 	document.querySelector('.banner-img').src='assets/images/slideshow/' + slides[index].image;
 	document.querySelector('#banner p').innerHTML=slides[index].tagLine;
+	document.querySelector('.dots').innerHTML=slides[index].divDots;
 	// alert('OK'); 
 }
+
+function loop() {
+	if (index > slides.length - 1) {
+		index = 0;
+	}
+	if (index < 0) {
+		index = slides.length - 1;
+	}
+}
+
+/* function loop() {
+	if (index > 3) {
+		index = 0;
+	}
+	if (index < 0) {
+		index = 3;
+	}
+} */
+
