c717ec02175f6d81b1ec6e078244ab777b5c0151
jdesiderio <desiderio.janyce@gmail.com>
2023-06-06 13:40:28 +0200

    V2 script : correction des dots

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,42 @@
 const arrowLeft = document.querySelector('.arrow_left');
 const arrowRight = document.querySelector('.arrow_right');
-
-let index = 0;
+const dots = document.querySelector(".dots");
 
 const slides = [
 	{
 		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>",
-		"divDots":'<div class="dot dot_selected"></div><div class="dot"></div><div class="dot"></div><div class="dot"></div>'
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
 		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-		"divDots":'<div class="dot"></div><div class="dot dot_selected"></div><div class="dot"></div><div class="dot"></div>'
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
 		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>",
-		"divDots":'<div class="dot"></div><div class="dot"></div><div class="dot dot_selected"></div><div class="dot"></div>'
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
 		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>",
-		"divDots":'<div class="dot"></div><div class="dot"></div><div class="dot"></div><div class="dot dot_selected"></div>'
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let index = 0;
 
 arrowLeft.addEventListener('click', imgChange);
 arrowRight.addEventListener('click', imgChange);
 
+slides.forEach ((element, index) => { 
+	const dot = document.createElement("div");
+  dot.classList.add("dot");
+  dots.appendChild(dot);
+	if (index === 0) {
+		dot.classList.add("dot_selected");
+	}
+}) ;
+
 function imgChange(e) {
-	console.log(e);
-	e.target.className;
 	const isArrowRight = e.target.className.includes('arrow_right');
-	console.log(isArrowRight);
 	
 	if (isArrowRight) {
 		index++;	
@@ -42,29 +45,20 @@ function imgChange(e) {
 		index--;
 	}
 
-	loop();
-	
-	document.querySelector('.banner-img').src='assets/images/slideshow/' + slides[index].image;
-	document.querySelector('#banner p').innerHTML=slides[index].tagLine;
-	document.querySelector('.dots').innerHTML=slides[index].divDots;
-	// alert('OK'); 
-}
-
-function loop() {
+	// gérer la circulaire 
 	if (index > slides.length - 1) {
 		index = 0;
 	}
 	if (index < 0) {
 		index = slides.length - 1;
 	}
+
+	// dots
+	document.querySelector('.dot_selected').className='dot';
+	const dot = document.querySelectorAll('.dot');
+	dot[index].className='dot dot_selected';
+
+	document.querySelector('.banner-img').src='assets/images/slideshow/' + slides[index].image;
+	document.querySelector('#banner p').innerHTML=slides[index].tagLine;
+
 }
-
-/* function loop() {
-	if (index > 3) {
-		index = 0;
-	}
-	if (index < 0) {
-		index = 3;
-	}
-} */
-
